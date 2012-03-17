20efa4c1524c20cfe5a2be0ba57fcb5882bf9c20
rajann44 <rajann44@gmail.com>
2012-03-17 12:00:00 +0100

    chore: use placeholder secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RubyOnRailsTaskManager::Application.config.secret_token = 'cebfafec9efb3a31062317bcbba45f7b5b52c8c617d57c1e761e0fa55ebee7497852b382cbf8571f3fa42bd1e36548b3e5258d67cfc18c9059be2f368d6f6945'
+#
+# IMPORTANT: Replace this value with a secure random string in production!
+RubyOnRailsTaskManager::Application.config.secret_token = 'please_change_me_to_a_real_secret_token'
